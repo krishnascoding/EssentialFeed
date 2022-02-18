217088897c1ed5828c49aa539ef2fc83867cea34
Krishna Ramachandran <krishnadrum@gmail.com>
2022-02-17 21:23:34 -0700

    Add FeedItem model

---
+++ EssentialFeed/FeedItem.swift
@@ -0,0 +1,15 @@
+//
+//  FeedItem.swift
+//  EssentialFeed
+//
+//  Created by Krishna Ramachandran on 2/17/22.
+//
+
+import Foundation
+
+struct FeedItem {
+    let id: UUID
+    let description: String?
+    let location: String?
+    let imageURL: URL
+}
